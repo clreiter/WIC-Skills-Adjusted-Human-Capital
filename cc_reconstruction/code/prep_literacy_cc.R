02e1e94105ddd3b63c902bd61509ae81b222715d
DilekYildiz <53214131+DilekYildiz@users.noreply.github.com>
2020-03-17 10:23:27 +0100

    Add files via upload

---
+++ cc_reconstruction/code/prep_literacy_cc.R
@@ -0,0 +1,11 @@
+rm(list = ls())
+
+library(tidyverse)
+
+d1 <- read.csv("./data/literacy_1970_2015_gender.csv")
+
+d2 <- d1 %>% 
+  rename(illiterate_prop = tot_ill_) %>% 
+  select(iso, year, illiterate_prop)
+
+write.csv(d2, "./data/df_illiterate_prop_cc_1970_2015.csv", row.names = F)
