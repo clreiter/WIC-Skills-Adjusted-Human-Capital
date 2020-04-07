23786096005a416adbe7037b551530c7501c9f5e
ozdemircaner <61315342+ozdemircaner@users.noreply.github.com>
2020-04-07 10:56:14 +0200

    Add files via upload

--- adj_fact/code/model_comparison.R
+++ adj_fact/code/model_comparison.R
@@ -1,41 +1,21 @@
 rm(list = ls())
 library(tidyverse)
-d1 <- read.csv("./results/results_adj_fac_1970_2015.csv") %>%
-  arrange(fit2) %>% 
-  add_rownames() %>% 
-  mutate(rowname2 = as.numeric(rowname)) %>% 
-  select(cc, iso, fit2, year, rowname2)
-
-d2 <- read.csv("./results/results_log_dummyyears.csv") %>%
-  arrange(fit1) %>% 
-  add_rownames() %>% 
-  mutate(rowname1 = as.numeric(rowname)) %>% 
-  select(cc, iso, fit1, year, rowname1)
+d1 <- read.csv("./results/results_adj_fac_1970_2015.csv")%>%
+  select(cc, iso, fit2, year)
+d2 <- read.csv("./results/results_log_dummyyears.csv")%>%
+  select(cc, iso, fit1, year)
 
 r1 <- d1 %>% 
   left_join(d2)
 
 p1 <- ggplot(r1, aes(x = fit1, y = fit2)) +
   geom_point() +
-  # geom_smooth() +
-  ggtitle("Model comparison") +
-  xlab("QAMYS estimates with age groups") +
-  ylab("Adjustment factor estimates with old dependency") +
-  geom_abline() +
+  geom_smooth() +
+  ggtitle("model comparison") +
+  xlab("samys estimates with age groups") +
+  ylab("adj factor estimates with old dependency") +
+  # geom_abline() +
   theme_bw()
 png("./results/model_comparison.png", width = 5, height = 5, res = 300, units = "in")
 print(p1)
-dev.off()
-
-
-p2 <- ggplot(r1, aes(x = rowname1, y = rowname2)) +
-  geom_point() +
-   # geom_smooth() +
-  ggtitle("Model comparison") +
-  xlab("Rank in regression with age groups") +
-  ylab("Rank in regression with old dependency") +
-  geom_abline() +
-  theme_bw()
-png("./results/model_comparison_rank.png", width = 5, height = 5, res = 300, units = "in")
-print(p2)
 dev.off()
